54851c4a92ca9ba266d33f112cc45d917515f959
rArtistaa <gabriellsbagetti@gmail.com>
2025-03-13 12:36:35 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -6,10 +5,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Gabriel Leão'
     })
-
-def contato(request):
-    return HttpResponse('CONTATO')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
